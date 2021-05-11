b6c82a6f74f8ce88bc8aa738c328b97189a3572e
yasserrz <yasseryuliant@gmail.com>
2021-05-11 23:40:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,25 +33,26 @@ Route::get('/basis','BasisController@index');
 /*
 Gejala
 */
-Route::get('/gejala','GejalaController@index');
-Route::get('/gejala/create','GejalaController@create');
-Route::get('/gejala/{gejala}','GejalaController@show');
-Route::post('/gejala','GejalaController@store');
-Route::delete('/gejala/{gejala}','GejalaController@destroy');
-Route::get('/gejala/{gejala}/edit','GejalaController@edit');
-Route::patch('/gejala/{gejala}','GejalaController@update');
+// Route::get('/gejala','GejalaController@index');
+// Route::get('/gejala/create','GejalaController@create');
+// Route::get('/gejala/{gejala}','GejalaController@show');
+// Route::post('/gejala','GejalaController@store');
+// Route::delete('/gejala/{gejala}','GejalaController@destroy');
+// Route::get('/gejala/{gejala}/edit','GejalaController@edit');
+// Route::patch('/gejala/{gejala}','GejalaController@update');
+Route::resource('kerusakan','KerusakanController');
 
 
 
 /*
 Kerusakanq
 */
-Route::get('/kerusakan','KerusakanController@index');
-Route::get('/kerusakan/create','KerusakanController@create');
-Route::get('/kerusakan/{kerusakan}','KerusakanController@show');
-Route::post('/kerusakan','KerusakanController@store');
-Route::delete('/kerusakan/{kerusakan}','KerusakanController@destroy');
-Route::get('/kerusakan/{kerusakan}/edit','KerusakanController@edit');
-Route::patch('/kerusakan/{kerusakan}','KerusakanController@update');
-
+// Route::get('/kerusakan','KerusakanController@index');
+// Route::get('/kerusakan/create','KerusakanController@create');
+// Route::get('/kerusakan/{kerusakan}','KerusakanController@show');
+// Route::post('/kerusakan','KerusakanController@store');
+// Route::delete('/kerusakan/{kerusakan}','KerusakanController@destroy');
+// Route::get('/kerusakan/{kerusakan}/edit','KerusakanController@edit');
+// Route::patch('/kerusakan/{kerusakan}','KerusakanController@update');
+Route::resource('kerusakan','KerusakanController');
 
